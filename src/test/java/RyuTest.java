b66d3abe96498152f822767bc3aa35508c624dfe
Nanoruri <kaby1217@gmail.com>
2023-11-30 19:12:39 +0900

    류주성의 (리스트)홀수 테스트 중력 500배 실행 2호

--- src/test/java/RyuTest.java
+++ src/test/java/RyuTest.java
@@ -9,8 +9,8 @@ import java.util.List;
 public class RyuTest {
     @Test
     public void ryuTest(){
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9);//이거 for문 돌려서..?
-
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 8 ,9, 10);//이거 for문 돌려서..?
+//        list.size() = 이 사이즈는 자기 내부에 있는 숫자의 개수를 반환함
 
 //        List<Integer> result = new ArrayList<Integer>();
 //        for (int i = 0; i < list.size() )
