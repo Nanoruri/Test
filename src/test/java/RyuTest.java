d7e5f5273258fdd45e07cc866951edd941a361a2
Nanoruri <kaby1217@gmail.com>
2023-11-30 20:32:02 +0900

    류주성의 (리스트)홀수 테스트 중력 500배 디버깅 5호

--- src/test/java/RyuTest.java
+++ src/test/java/RyuTest.java
@@ -9,16 +9,17 @@ import static javax.swing.UIManager.get;
 
 public class RyuTest {
     @Test
-    public void ryuTest(){
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9, 10);//이거 for문 돌려서..?
+    public void ryuTest() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);//이거 for문 돌려서..?
 //        list.size() = 이 사이즈는 자기 내부에 있는 것의 개수를 반환함
 
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (int i = 0; i < list.size() ; i++ ){//지금 size는 10?
-            if (i % 2 ==0){//2로 나눠서 나머지가 0과 같으면...result값에 추가 하면 될듯 = 짝수만 담겨짐
+        for (int i = 0; i < list.size(); i++) {//지금 size는 10?
+            if (i % 2 == 0) {//2로 나눠서 나머지가 0과 같으면...result값에 추가 하면 될듯 = 짝수만 담겨짐
             }
 
-        System.out.println();
+            System.out.println();
         }
     }
+}
 
