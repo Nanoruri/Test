8ade483328ea737e1fe5c0bacf414152e6e7a665
Nanoruri <kaby1217@gmail.com>
2023-11-30 18:27:53 +0900

    류주성의 (리스트)홀수 테스트 중력 500배

--- src/test/java/RyuTest.java
+++ src/test/java/RyuTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,9 +10,6 @@ public class RyuTest {
     @Test
     public void ryuTest(){
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9, 10);
-        List<Integer> result = list.stream()//와우, 스트림이라는 반복 연산을 자동으로 해주는 개쩌는 친구가 있구나
-                .filter(number -> number % 2 == 0)//연산 식이고
-                .collect(Collectors.toList());//거르고 남은 짝수친구들을 collect해서 리스트로 다시 반영해줌
         System.out.println(result);
     }
 }
