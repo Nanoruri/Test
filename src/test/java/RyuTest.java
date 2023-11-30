fc3702025860ed2f9ee14a3b0283ae72c5102156
Nanoruri <kaby1217@gmail.com>
2023-11-30 19:10:11 +0900

    류주성의 (리스트)홀수 테스트 중력 500배 실행 1호

--- src/test/java/RyuTest.java
+++ src/test/java/RyuTest.java
@@ -3,13 +3,21 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 
 public class RyuTest {
     @Test
     public void ryuTest(){
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9, 10);
-        System.out.println(result);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9, 10);//이거 for문 돌려서..?
+
+
+//        List<Integer> result = new ArrayList<Integer>();
+//        for (int i = 0; i < list.size() )
+//            if (holsu % 2 ==0){//2로 나눠서 나머지가 0과 같으면...
+//                result.
+//        }
+
+        System.out.println(list.size());
     }
 }
